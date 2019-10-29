fdb47112eaec2262fdb2f0602e4266debee5a9c0
xomyachok_shaolin <xomyackok8@gmail.com>
2019-10-29 17:51:01 +0500

    HW1_2

--- app/src/main/java/com/example/sudokuhw/About.java
+++ app/src/main/java/com/example/sudokuhw/About.java
@@ -2,13 +2,33 @@ package com.example.sudokuhw;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class About extends AppCompatActivity {
 
+    TextView title;
+    TextView inf;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        getUIItems();
+        setFonts();
+    }
+
+    public void getUIItems() {
+
+        title = (TextView) findViewById(R.id.abooutTitle);
+        inf = (TextView) findViewById(R.id.aboutTV);
+
+    }
+    private void setFonts() {
+        Typeface type = Typeface.createFromAsset(getAssets(), "font1.otf");
+        title.setTypeface(type);
+        inf.setTypeface(type);
+
     }
 }
--- app/src/main/java/com/example/sudokuhw/MainActivity.java
+++ app/src/main/java/com/example/sudokuhw/MainActivity.java
@@ -2,13 +2,61 @@ package com.example.sudokuhw;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button aboutBtn;
+    Button exitBtn;
+    Button newGameBtn;
+    Button continueBtn;
+
+    TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getUIItems();
+        setFonts();
+    }
+
+    public void getUIItems() {
+        aboutBtn = (Button) findViewById(R.id.btnAbout);
+        exitBtn = (Button) findViewById(R.id.btnExit);
+        newGameBtn = (Button) findViewById(R.id.btnNewGame);
+        continueBtn = (Button) findViewById(R.id.btnContinue);
+
+        title = (TextView) findViewById(R.id.title);
+    }
+
+    public void continueGame(View view) {
+
+    }
+    public void newGame(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+    public void aboutGame(View view) {
+        Intent intent = new Intent(this, About.class);
+        startActivity(intent);
+    }
+    public void exitGame(View view) {
+        finish();
+    }
+    private void setFonts() {
+        Typeface type = Typeface.createFromAsset(getAssets(), "font1.otf");
+
+        title.setTypeface(type);
+        aboutBtn.setTypeface(type);
+        exitBtn.setTypeface(type);
+        newGameBtn.setTypeface(type);
+        continueBtn.setTypeface(type);
     }
 }
